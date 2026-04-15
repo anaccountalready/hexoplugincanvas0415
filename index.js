d6c878404a85c47622a0ef71e56e908ee837c8c8
ALANDEWI <1599138016@qq.com>
2026-04-15 18:25:46 +0800

    02

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 
+let cssContent = '';
+let jsContent = '';
+let resourcesInjected = false;
+
+try {
+  const cssPath = path.join(__dirname, 'assets', 'canvas-doodle.css');
+  const jsPath = path.join(__dirname, 'assets', 'canvas-doodle.js');
+  cssContent = fs.readFileSync(cssPath, 'utf8');
+  jsContent = fs.readFileSync(jsPath, 'utf8');
+} catch (e) {
+  console.error('hexo-canvas-doodle: Failed to load assets:', e.message);
+}
+
 hexo.extend.tag.register('canvas', function(args) {
   const canvasId = args[0] || 'doodle-canvas-' + Math.random().toString(36).substr(2, 9);
   const width = args[1] || '800';
   const height = args[2] || '400';
   
+  let resourcesHtml = '';
+  if (!resourcesInjected) {
+    resourcesInjected = true;
+    resourcesHtml = `
+<style>
+${cssContent}
+</style>
+<script>
+${jsContent}
+</script>
+`;
+  }
+  
   const canvasHtml = `
+${resourcesHtml}
 <div class="hexo-canvas-doodle-container" data-canvas-id="${canvasId}">
   <canvas id="${canvasId}" width="${width}" height="${height}" class="hexo-canvas-doodle"></canvas>
   <div class="hexo-canvas-doodle-controls">
@@ -19,15 +46,4 @@ hexo.extend.tag.register('canvas', function(args) {
 `;
   
   return canvasHtml;
-}, { ends: false });
-
-hexo.extend.filter.register('after_generate', function() {
-  const cssPath = path.join(__dirname, 'assets', 'canvas-doodle.css');
-  const jsPath = path.join(__dirname, 'assets', 'canvas-doodle.js');
-  
-  const cssContent = fs.readFileSync(cssPath, 'utf8');
-  const jsContent = fs.readFileSync(jsPath, 'utf8');
-  
-  hexo.extend.injector.register('head_end', `<style>${cssContent}</style>`);
-  hexo.extend.injector.register('body_end', `<script>${jsContent}</script>`);
-});
+}, { ends: false, async: false });
